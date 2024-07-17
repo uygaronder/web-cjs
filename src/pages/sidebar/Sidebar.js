7e612e86b171061ec3531f7175c3595149c5c384
uygaronder <uonder50@gmail.com>
2024-07-17 17:44:24 +0300

    style: Update top bar styling and search bar in sidebar

--- src/pages/sidebar/Sidebar.js
+++ src/pages/sidebar/Sidebar.js
@@ -6,27 +6,29 @@ import Menu from "../../shared/assets/svg/menu.svg";
 import Search from "../../shared/assets/svg/search.svg";
 
 const Sidebar = () => {
+  const inputRef = React.createRef();
+  const handleSearchBarClick = () => {
+    inputRef.current.focus();
+  };
+
   return (
     <div className="sidebar">
-      <div className="sidebar-upper">
-        <div className="sidebar-dropdown-menu-container">
-            <img src={Menu} alt="Menu" />
-            <div className="sidebar-dropdown-menu">
-                <ul>
-                    <li>
-                        <Link to="/chat">Chat</Link>
-                    </li>
-                </ul>
-            </div>
+      <div className="sidebar-header">
+        <div className="sidebar-avatar">
+          <img src="https://placehold.co/100x100" alt="Avatar" />
         </div>
-        <div className="sidebar-search-bar">
+        <div className="sidebar-menu-buttons">
+        </div>
+      </div>
+      <div className="sidebar-upper">
+        <div className="sidebar-search-bar" onClick={handleSearchBarClick}>
           <img src={Search} alt="Search" />
-          <input type="text" placeholder="Search" />
+          <input type="text" placeholder="Search" ref={inputRef} />
         </div>
       </div>
       <div className="sidebar-body">
         <ul className="sidebar-chats">
-          <Link to="/chat" className="sidebar-chat">
+          <Link to="/c/chat" className="sidebar-chat">
             <div className="chat-avatar">
               <img
                 src="https://placehold.co/100x100"
