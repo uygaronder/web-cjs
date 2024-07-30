6f7c704f610376e0a6b97f6ffc52c143ebfef404
uygaronder <uonder50@gmail.com>
2024-07-30 23:32:12 +0300

    style: Add vertical divider in NewChatRoom component

--- src/pages/sidebar/components/NewChatRoom.js
+++ src/pages/sidebar/components/NewChatRoom.js
@@ -41,6 +41,7 @@ const NewChatRoom = ( {closePrompt} ) => {
                 onChange={handleInputChange}
             />
         </div>
+        <span className='verticalDivider'></span>
         </>
     );
 };
