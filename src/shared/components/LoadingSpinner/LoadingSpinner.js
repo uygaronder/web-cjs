1497ade6ec74685c90d56924ccf2cf79d5b1e692
Uygar <uonder50@gmail.com>
2024-11-03 21:51:13 +0300

    feat: Add LoadingSpinner component with CSS and SCSS styling

---
+++ src/shared/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/LoadingSpinner.css';
+
+const LoadingSpinner = () => {
+    return (
+        <div className="loading-spinner">
+            <div className="spinner">
+                Loading...
+            </div>
+        </div>
+    );
+};
+
+export default LoadingSpinner;
